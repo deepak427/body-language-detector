5de8e32fea4c39ae354ddf9c8eddac1e4697c280
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-02-02 12:25:12 +0530

    created pipeline and trained model

--- main.py
+++ main.py
@@ -87,91 +87,127 @@ mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
 #     csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 #     csv_writer.writerow(landmarks)
 
-class_name = 'happy'
+# class_name = 'victorious'
 
-cap = cv2.VideoCapture(0)
-# Initiate holistic model
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+# cap = cv2.VideoCapture(0)
+# # Initiate holistic model
+# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
 
-    while cap.isOpened():
-        ret, frame = cap.read()
+#     while cap.isOpened():
+#         ret, frame = cap.read()
 
-        # Recolor Feed
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+#         # Recolor Feed
+#         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         image.flags.writeable = False
 
-        # Make Detections
-        results = holistic.process(image)
-        # print(results.face_landmarks)
+#         # Make Detections
+#         results = holistic.process(image)
+#         # print(results.face_landmarks)
 
-        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+#         # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
 
-        # Recolor image back to BGR for rendering
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#         # Recolor image back to BGR for rendering
+#         image.flags.writeable = True
+#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # 1. Draw face landmarks
-        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(
-                                      color=(80, 110, 10), thickness=1, circle_radius=1),
-                                  mp_drawing.DrawingSpec(
-                                      color=(80, 256, 121), thickness=1, circle_radius=1)
-                                  )
+#         # 1. Draw face landmarks
+#         mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(80, 110, 10), thickness=1, circle_radius=1),
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(80, 256, 121), thickness=1, circle_radius=1)
+#                                   )
 
-        # 2. Right hand
-        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(
-                                      color=(80, 22, 10), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(
-                                      color=(80, 44, 121), thickness=2, circle_radius=2)
-                                  )
+#         # 2. Right hand
+#         mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(80, 22, 10), thickness=2, circle_radius=4),
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(80, 44, 121), thickness=2, circle_radius=2)
+#                                   )
 
-        # 3. Left Hand
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(
-                                      color=(121, 22, 76), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(
-                                      color=(121, 44, 250), thickness=2, circle_radius=2)
-                                  )
+#         # 3. Left Hand
+#         mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(121, 22, 76), thickness=2, circle_radius=4),
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(121, 44, 250), thickness=2, circle_radius=2)
+#                                   )
 
-        # 4. Pose Detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(
-                                      color=(245, 117, 66), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(
-                                      color=(245, 66, 230), thickness=2, circle_radius=2)
-                                  )
-        # Export coordinates
-        try:
-            # Extract Pose landmarks
-            pose = results.pose_landmarks.landmark
-            pose_row = list(np.array(
-                [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+#         # 4. Pose Detections
+#         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(245, 117, 66), thickness=2, circle_radius=4),
+#                                   mp_drawing.DrawingSpec(
+#                                       color=(245, 66, 230), thickness=2, circle_radius=2)
+#                                   )
+#         # Export coordinates
+#         try:
+#             # Extract Pose landmarks
+#             pose = results.pose_landmarks.landmark
+#             pose_row = list(np.array(
+#                 [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
 
-            # Extract Face landmarks
-            face = results.face_landmarks.landmark
-            face_row = list(np.array(
-                [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
+#             # Extract Face landmarks
+#             face = results.face_landmarks.landmark
+#             face_row = list(np.array(
+#                 [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
 
-            # Concate rows
-            row = pose_row+face_row
+#             # Concate rows
+#             row = pose_row+face_row
 
-            # Append class name
-            row.insert(0, class_name)
+#             # Append class name
+#             row.insert(0, class_name)
 
-            # Export to CSV
-            with open('coords.csv', mode='a', newline='') as f:
-                csv_writer = csv.writer(
-                    f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                csv_writer.writerow(row)
+#             # Export to CSV
+#             with open('coords.csv', mode='a', newline='') as f:
+#                 csv_writer = csv.writer(
+#                     f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#                 csv_writer.writerow(row)
 
-        except:
-            pass
+#         except:
+#             pass
 
-        cv2.imshow('Raw Webcam Feed', image)
+#         cv2.imshow('Raw Webcam Feed', image)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
 
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
+
+# Train custom model using scikit learn
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('coords.csv')
+df.head()
+
+X = df.drop('class',axis=1)
+y = df['class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
+
+# Train Machine Learning Classification Model
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+from sklearn.metrics import classification_report
+
+pipelines = {
+    'lr':make_pipeline(StandardScaler(), LogisticRegression()),
+    'rc':make_pipeline(StandardScaler(), RidgeClassifier()),
+    'rf':make_pipeline(StandardScaler(), RandomForestClassifier()),
+    'gb':make_pipeline(StandardScaler(), GradientBoostingClassifier()),
+}
+
+fit_models = {}
+for algo, pipeline in pipelines.items():
+    model = pipeline.fit(X_train, y_train)
+    fit_models[algo] = model
+
+print(classification_report(y_test, fit_models['lr'].predict(X_test)))
